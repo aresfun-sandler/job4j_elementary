3381840622f3dd8061d593df8c690177125be8fa
aresfun-sandler <alex-sandler@mail.ru>
2021-03-07 14:18:07 +0300

    4.2. Тернарное сравнение [#479471]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int more = left > right ? left : right;
-        return more;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
