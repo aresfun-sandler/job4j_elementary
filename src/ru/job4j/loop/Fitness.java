0011f2f02786197d1a7f2f9fa476e0c5feda8572
aresfun-sandler <alex-sandler@mail.ru>
2021-03-09 17:44:14 +0300

    5.4. Протеиновая диета [#479568]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -1,11 +1,11 @@
 package ru.job4j.loop;
 
 public class Fitness {
-    public static int calc(int ivan, int nik) {
+    public static int calc(int ivan, int nikol) {
         int month = 0;
-        while (ivan < nik) {
+        while (ivan < nikol) {
         ivan *= 3;
-        nik *= 2;
+        nikol *= 2;
         month ++;
         }
         return month;
