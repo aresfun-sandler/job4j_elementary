c4e41be68f2c23fdaf257ae00fb95481c3a27c2f
aresfun-sandler <alex-sandler@mail.ru>
2021-03-10 14:27:38 +0300

    5.5. Простое число [#479571]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,4 +1,19 @@
 package ru.job4j.loop;
 
 public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+            prime = false;
+            break;
+            }
+        }
+        return prime;
+    }
+
+    public static void main(String[] args) {
+        boolean out = CheckPrimeNumber.check(-2);
+        System.out.println(out);
+    }
 }
