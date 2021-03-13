28b7602258fe6d405b8936613937c79c79590330
aresfun-sandler <alex-sandler@mail.ru>
2021-03-13 14:07:03 +0300

    6.4.1. Сравнить последние элементы двух массивов [#479499]

--- test/ru/job4j/array/EqLastTest.java
+++ test/ru/job4j/array/EqLastTest.java
@@ -10,8 +10,8 @@ public class EqLastTest {
 
     @Test
     public void whenEq() {
-        int[] left = {1, 2, 5};
-        int[] right = {5, 4, 5};
+        int[] left = {1, 2, 4};
+        int[] right = {5, 4, 4};
         assertThat(check(left, right), is(true));
     }
 
