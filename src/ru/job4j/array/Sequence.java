f2848b31b210179d5ba7cb3198adf6b1600ab57f
aresfun-sandler <alex-sandler@mail.ru>
2021-03-14 15:44:01 +0300

    6.6.1. Двухмерный массив. Вывод на консоль. [#479498]

---
+++ src/ru/job4j/array/Sequence.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 9;
+        array[0][1] = 8;
+        array[0][2] = 7;
+        array[1][0] = 6;
+        array[1][1] = 5;
+        array[1][2] = 4;
+        array[2][0] = 3;
+        array[2][1] = 2;
+        array[2][2] = 1;
+    }
+}
