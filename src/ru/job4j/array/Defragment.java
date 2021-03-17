dbd329fa873ffad8e30ff937c55219d12db07cfb
aresfun-sandler <alex-sandler@mail.ru>
2021-03-17 03:45:32 +0300

    6.8. Дефрагментация массива [#479506]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -4,13 +4,11 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == null) {
-                int nullIndex = i;
-                for (int j = array.length - 1; j != i; j--) {
+                for (int j = i; j < array.length; j++) {
                     if (array[j] != null) {
-                        int notNullIndex = j;
-                        String temp = array[notNullIndex];
-                        array[notNullIndex] = array[nullIndex];
-                        array[nullIndex] = temp;
+                        String temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
                     }
                   }
                 }
